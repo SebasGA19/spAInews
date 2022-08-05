d36f890e8b671d6df609298108c8ed8d77587bb0
Antonio <antoniojosedonishung@gmail.com>
2022-08-05 18:34:27 -0500

    CORS support

--- api/internal/web/engine.go
+++ api/internal/web/engine.go
@@ -2,11 +2,34 @@ package web
 
 import (
 	"github.com/SebasGA19/spAInews/api/internal/controller"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
 
 func NewEngine(c *controller.Controller) *gin.Engine {
 	engine := gin.Default()
+	engine.Use(
+		cors.New(
+			cors.Config{
+				AllowOrigins: []string{"*"},
+				AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+				AllowHeaders: []string{
+					"Origin",
+					"Content-Type",
+					"Content-Length",
+					SessionHeader,
+					"Authorization"},
+				ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+				AllowCredentials: true,
+				AllowOriginFunc: func(origin string) bool {
+					return true
+				},
+				MaxAge: 12 * time.Hour,
+			},
+		),
+	)
 	backend := NewBackend(c)
 	engine.PUT(RegisterURI, backend.Register)
 	engine.POST(ConfirmAccountURI, backend.ConfirmAccount)
