15209fb3347df5f9645784611228bc22dcce82f6
Antonio <antoniojosedonishung@gmail.com>
2022-08-13 10:40:53 -0500

    Update dev-databases.ps1

--- dev-databases.ps1
+++ dev-databases.ps1
@@ -1,3 +1,6 @@
+# Remove dependency folders
+Remove-Item -Recurse -Force "mariadb-data"
+Remove-Item -Recurse -Force "mongo-data"
 # Remove any running instance
 docker-compose -f .\compose-dev-back.yaml down --remove-orphans -v
 # Start the new dev instance locally
