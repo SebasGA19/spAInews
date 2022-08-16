6947661fcbccb538e8e486eed882233f730477d2
Antonio <antoniojosedonishung@gmail.com>
2022-08-16 15:09:48 -0500

    Example custom html

--- frontend-2/src/components/page.svelte
+++ frontend-2/src/components/page.svelte
@@ -8,8 +8,10 @@ let darkMode = false;
 <button on:click="{() => darkMode = !darkMode}">{darkMode ? "Light" : "Dark"}</button>
 {#if darkMode}
 <link rel="stylesheet" href="/vendor/bootstrap-5.2.0-dist/css/bootstrap.min-slate.css"/>
+<link rel="stylesheet" href="/css/dark.css"/>
 {:else}
 <link rel="stylesheet" href="/vendor/bootstrap-5.2.0-dist/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="/css/light.css"/>
 {/if}
 <Navbar bind:darkMode/>
 <slot></slot>
