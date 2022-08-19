4d9d1541c466d119b603b03a7597a6288eb758e1
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2022-08-19 17:36:52 -0500

    Confirm Email, Password, Register

---
+++ frontend-2/src/routes/confirmchangeemail.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { change_email } from '../api/change-email';
+	import Page from '../components/page.svelte';
+
+	let confirmCode;
+	let successMessage, errorMessage;
+
+	function handleConfirm() {
+		change_email(confirmCode)
+			.then(() => {
+				successMessage = 'Email changed succesfully';
+			})
+			.catch((error) => {
+				errorMessage = error;
+			});
+	}
+</script>
+
+<Page>
+	<div class="container text-center w-25">
+		<h3>Confirm Change email</h3>
+		{#if errorMessage}
+			<div class="alert alert-danger" role="alert">
+				{errorMessage}
+			</div>
+		{/if}
+		{#if successMessage}
+			<div class="alert alert-danger" role="alert">
+				{successMessage}
+			</div>
+		{/if}
+		<form on:submit|preventDefault={handleConfirm}>
+			<div class="mb-3">
+				<label for="confirm-code" class="form-label">Confirmation code</label>
+				<input
+					required
+					type="text"
+					placeholder="confirm-code"
+					bind:value={confirmCode}
+					class="form-control"
+				/>
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+</Page>
---
+++ frontend-2/src/routes/confirmchangepassword.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { change_password } from '../api/change-password';
+	import Page from '../components/page.svelte';
+
+	let confirmCode;
+	let successMessage, errorMessage;
+
+	function handleConfirm() {
+		change_password(confirmCode)
+			.then(() => {
+				successMessage = 'Password changed succesfully';
+			})
+			.catch((error) => {
+				errorMessage = error;
+			});
+	}
+</script>
+
+<Page>
+	<div class="container text-center w-25">
+		<h3>Confirm Change password</h3>
+		{#if errorMessage}
+			<div class="alert alert-danger" role="alert">
+				{errorMessage}
+			</div>
+		{/if}
+		{#if successMessage}
+			<div class="alert alert-danger" role="alert">
+				{successMessage}
+			</div>
+		{/if}
+		<form on:submit|preventDefault={handleConfirm}>
+			<div class="mb-3">
+				<label for="confirm-code" class="form-label">Confirmation code</label>
+				<input
+					required
+					type="text"
+					placeholder="confirm-code"
+					bind:value={confirmCode}
+					class="form-control"
+				/>
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+</Page>
