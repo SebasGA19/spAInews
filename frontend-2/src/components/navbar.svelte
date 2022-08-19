e60600af96e12d2e765b57979b02beaf945bac93
Antonio <antoniojosedonishung@gmail.com>
2022-08-19 16:56:06 -0500

    Dark mode switch

--- frontend-2/src/components/navbar.svelte
+++ frontend-2/src/components/navbar.svelte
@@ -1,14 +1,19 @@
 <script>
 import { session } from "../stores/session";
+import { darkMode } from "../stores/dark-mode"
 
 import Login from "./login.svelte";
 import Register from "./register.svelte";
 
-export let darkMode = false;
+let darkModeChecked = $darkMode === 'dark';
 
+$ : {
+    darkModeChecked;
+    $darkMode = darkModeChecked ? 'dark' : 'light';
+}
 </script>
 
-<nav class="navbar navbar-expand-lg {darkMode ? 'bg-primary navbar-dark' : 'bg-light'}">
+<nav class="navbar navbar-expand-lg {darkModeChecked ? 'navbar-dark bg-dark' : 'bg-light'}">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -21,6 +26,14 @@ export let darkMode = false;
             </ul>
             <div class="d-flex" role="search">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item align-middle" style="margin: auto; margin-right: 30px;">
+                        <div class="form-check form-switch">
+                            <label class="form-check-label">
+                                {darkModeChecked ? "Dark" : "Light"}
+                                <input class="form-check-input" type="checkbox" role="switch" bind:checked="{darkModeChecked}">
+                            </label>
+                        </div>
+                    </li>
                     {#if $session !== ''}
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
--- frontend-2/src/components/page.svelte
+++ frontend-2/src/components/page.svelte
@@ -1,18 +1,17 @@
 <script>
+import { darkMode } from "../stores/dark-mode";
 import Footer from "./footer.svelte";
 import Navbar from "./navbar.svelte";
 
-let darkMode = false;
 </script>
 
-<button on:click="{() => darkMode = !darkMode}">{darkMode ? "Light" : "Dark"}</button>
-{#if darkMode}
-<link rel="stylesheet" href="/vendor/bootstrap-5.2.0-dist/css/bootstrap.min-slate.css"/>
+{#if $darkMode === 'dark'}
+<link rel="stylesheet" href="/vendor/bootstrap-5.2.0-dist/css/bootstrap.min-darkly.css"/>
 <link rel="stylesheet" href="/css/dark.css"/>
 {:else}
-<link rel="stylesheet" href="/vendor/bootstrap-5.2.0-dist/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="/vendor/bootstrap-5.2.0-dist/css/bootstrap.min-litera.css"/>
 <link rel="stylesheet" href="/css/light.css"/>
 {/if}
-<Navbar bind:darkMode/>
+<Navbar/>
 <slot></slot>
 <Footer/>
