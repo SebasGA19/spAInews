6b99ef0230eb8673c9eeada4d968ab8cac2327f3
Antonio <antoniojosedonishung@gmail.com>
2022-09-24 15:40:44 -0500

    Migration to production ready compose

--- setup-dev.ps1
+++ setup-dev.ps1
@@ -2,6 +2,6 @@
 Remove-Item -Recurse -Force "mariadb-data"
 Remove-Item -Recurse -Force "mongo-data"
 # Remove any running instance
-docker-compose -f .\compose-dev-front.yaml down --remove-orphans -v
+docker-compose -f .\docker-compose.yaml down --remove-orphans -v
 # Start the new dev instance locally
-docker-compose -f .\compose-dev-front.yaml up -d --build
+docker-compose -f .\docker-compose.yaml up -d --build
