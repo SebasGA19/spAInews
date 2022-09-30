8c8f9af98ba525b284645f851ce06fd528b77e07
Antonio <antoniojosedonishung@gmail.com>
2022-09-30 12:39:53 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ mkdir -p /opt/spAInews
 docker-compose -f ./docker-compose.yaml down --remove-orphans
 docker-compose -f ./docker-compose.yaml up -d --build
 chmod 777 spainews.sh
-cp spainews.sh /etc/cron.daily
+cp spainews.sh /etc/cron.daily/spainews
